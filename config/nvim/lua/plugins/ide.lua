18154af7972123c3eecef4c0d773849154117f22
Joe Johnson <joe.johnson3909@gmail.com>
2026-08-19 13:59:53 +0000

    Add <CR> keymap for lsp accept suggestion

--- config/nvim/lua/plugins/ide.lua
+++ config/nvim/lua/plugins/ide.lua
@@ -239,6 +239,7 @@ return {
 			keymap = {
 				preset = "default",
 				["<Tab>"] = { "select_and_accept", "fallback" },
+				["<CR>"] = { "select_and_accept", "fallback" },
 			},
 			appearance = { nerd_font_variant = "mono" },
 			completion = {
