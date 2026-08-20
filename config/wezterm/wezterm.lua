ec0d57df95631400d304d846f1ac7fe60eb14e13
Joe Johnson <joe.johnson3909@gmail.com>
2026-08-20 17:39:11 +0100

    fix bugs in wezterm config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -31,8 +31,8 @@ config.underline_thickness = 1
 
 -- Make Alt+<key> send proper escape sequences (so <A-j>/<A-k> work in nvim)
 -- instead of Windows treating Left Alt as a compose/dead key.
-config.send_composed_key_when_left_alt_is_down = false
-config.send_composed_key_when_right_alt_is_down = false
+config.send_composed_key_when_left_alt_is_pressed = false
+config.send_composed_key_when_right_alt_is_pressed = false
 
 -- --- Keybindings (add your own) ---
 config.leader = { key = "b", mods = "CTRL" }
@@ -82,7 +82,7 @@ config.key_tables = {
 		{ key = "j", action = wezterm.action.AdjustPaneSize({ "Down", 3 }) },
 		{ key = "k", action = wezterm.action.AdjustPaneSize({ "Up", 3 }) },
 		{ key = "l", action = wezterm.action.AdjustPaneSize({ "Right", 3 }) },
-		{ get = "Escape", action = "PopKeyTable" },
+		{ key = "Escape", action = "PopKeyTable" },
 	},
 }
 
